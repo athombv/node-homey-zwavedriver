fe0a81a0418f252c78a97dad48da133d90fd1de1
Robin Bolscher <robinbolscher@gmail.com>
2020-01-21 09:49:47 +0100

    Fixes eslint config and code formatting

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ module.exports.ZwaveDevice = require('./lib/ZwaveDevice.js');
 module.exports.ZwaveLockDevice = require('./lib/ZwaveLockDevice.js');
 module.exports.ZwaveMeteringDevice = require('./lib/ZwaveMeteringDevice.js');
 module.exports.ZwaveLightDevice = require('./lib/ZwaveLightDevice.js');
-
-module.exports.ZigBeeDevice = require('./lib/zigbee/ZigBeeDevice.js');
-module.exports.ZigBeeLightDevice = require('./lib/zigbee/ZigBeeLightDevice.js');
-module.exports.ZigBeeXYLightDevice = require('./lib/zigbee/ZigBeeXYLightDevice.js');
