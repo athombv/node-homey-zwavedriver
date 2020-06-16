87b535e28b369dbf8c04f8877f0b707c7c560432
Robin Bolscher <robinbolscher@gmail.com>
2020-06-16 11:20:28 +0200

    revert: remove `ZwaveMeteringDevice` and `ZwaveLockDevice`

--- index.js
+++ index.js
@@ -3,6 +3,4 @@
 module.exports.Util = require('./lib/util');
 
 module.exports.ZwaveDevice = require('./lib/ZwaveDevice.js');
-module.exports.ZwaveLockDevice = require('./lib/ZwaveLockDevice.js');
-module.exports.ZwaveMeteringDevice = require('./lib/ZwaveMeteringDevice.js');
 module.exports.ZwaveLightDevice = require('./lib/ZwaveLightDevice.js');
