5aad91cecfc3afc6bcb979de7c2b674575f474de
Robin Bolscher <robinbolscher@gmail.com>
2026-03-11 09:55:38 +0100

    chore: fix lint error

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 module.exports.Util = require('./lib/util');
 
-module.exports.ZwaveDevice = require('./lib/ZwaveDevice.js');
-module.exports.ZwaveLightDevice = require('./lib/ZwaveLightDevice.js');
+module.exports.ZwaveDevice = require('./lib/ZwaveDevice');
+module.exports.ZwaveLightDevice = require('./lib/ZwaveLightDevice');
